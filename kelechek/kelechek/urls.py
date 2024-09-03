8f8be63bd3f91c7944cfb103a617f8568238f487
erlan <erlan.kubanychbekov.000@gmail.com>
2024-09-03 16:30:40 +0600

    Added Swager

--- kelechek/kelechek/urls.py
+++ kelechek/kelechek/urls.py
@@ -5,11 +5,31 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
+from rest_framework import permissions
+
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="API For Kelechek",
+        default_version='v1',
+        description="Your Kelechek in your hands",
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(email="aycanmuratbekova@gmail.com"),
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+    permission_classes=[permissions.AllowAny,],
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('user.urls')),
-    path('api/', include('kiyal.urls')),
+    path('api/user/', include('user.urls')),
+    path('api/kiyal/', include('kiyal.urls')),
+
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
